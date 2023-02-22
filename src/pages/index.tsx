a7b0d57da2bc0f9badcb5a768343a489c0866b7c
Dylan DesRosier <dylandesrosier@gmail.com>
2023-02-22 10:27:51 -0400

    v1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,70 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Button, { ButtonColor } from "../components/Button";
-import { Header } from "../components/Header";
+import { Page } from "../components/Page";
+import Link from "next/link";
+import { PoweredByPoolTogether } from "../components/PoweredByPoolTogether";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Head>
-        <title>Alpha</title>
-        <meta name="description" content="Alpha" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Page
+      title="PoolTogether Delegation Donation"
+      description="Delegate your chances to win to another wallet while maintaining full custody of your funds."
+    >
+      <div className="flex flex-col gap-8">
+        <Hero />
+        <Explainer />
+      </div>
+    </Page>
+  );
+};
 
-      <Header />
-      <main className="px-4 max-w-7xl mx-auto"></main>
+const Hero = () => {
+  return (
+    <div className="hero py-10 bg-base-200 rounded-xl">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Make an impact, risk free</h1>
+          <p className="py-6">
+            Delegate your chances to win prizes to charities and causes you care
+            about while maintaining full custody of your funds.
+          </p>
+          <Link href="/explore" className="">
+            <button className="btn btn-primary mb-4">Get Started</button>
+          </Link>
+          <PoweredByPoolTogether />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Explainer = () => {
+  return (
+    <div className="flex flex-col md:flex-row gap-8">
+      <div className="w-full card bg-base-300">
+        <div className="card-body ">
+          <ul className="steps steps-vertical text-lg md:h-96">
+            <li className="step step-secondary text-left">
+              Deposit the Prize Pool of your choice on PoolTogether, the
+              Ethereum worlds best prize savings account.
+            </li>
+            <li className="step step-secondary text-left">
+              Direct your chances to win to a charity or cause you care about by
+              delegating to them on this website.
+            </li>
+            <li className="step step-secondary text-left">
+              Wait for them to win prizes! Anyone can claim the prizes on their
+              behalf to ensure they receive their winnings.
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="mockup-window border border-base-200 bg-base-300 w-full h-96 md:h-auto">
+        <iframe
+          className="flex w-full h-full"
+          src="https://pooltogether.com"
+          title="PoolTogether"
+        />
+      </div>
     </div>
   );
 };
